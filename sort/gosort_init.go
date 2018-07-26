2ad333db1e931287962bf8b58c1af11e632edacc
Jenazads <felipe.moreno.vera@gmail.com>
2018-07-26 04:58:59 -0500

    init sort

--- sort/gosort_init.go
+++ sort/gosort_init.go
@@ -3,7 +3,6 @@ package gosort
 import "fmt";
 
 func init() {
-  fmt.Println("\n\n====> goSort version 1.0.1 last update 19/06/2018 \n")
-  fmt.Println("====> Init the GoSort for Sorting Algorithms library :D\n")
-  fmt.Println("====> by Jenazads.\n\n")
+  fmt.Println("====> Init GoSort, a Sorting Algorithms Library :D")
+  fmt.Println("====> by Jenazads.\n")
 }
